4579074070b3b2f2a759bf7095ac9c1c65ac8c5f
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2024-10-09 09:20:08 +0200

    Add files via upload

---
+++ klasaArrayList/klasaArrayList/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace klasaArrayList
+{
+    public partial class Form1 : Form
+    {
+        ArrayList listaAutomobila = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonUnesi_Click(object sender, EventArgs e)
+        {
+            Automobil a = new Automobil(textBoxNovi.Text);
+            listaAutomobila.Add(a);
+            richTextBox1.AppendText(listaAutomobila.IndexOf(a) + 1 + 
+                ". " + a.DajMarku() + "\n");
+        }
+    }
+}
